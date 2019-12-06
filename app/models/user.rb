3ecaa6e84f4aae9a14652a3005daea74c2090b9d
David Elì <davidelimg1@gmail.com>
2019-12-05 19:09:07 -0600

    Adding name and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 60 }
+  validates :email, presence: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
 end
