43261d4d9ce5fd0c58ba4713e5f6e40df96e99a0
David Elì <davidelimg1@gmail.com>
2020-01-15 18:24:13 -0600

    Added secur_password, and index to email users, Added many gems

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.4'
 
+gem 'bcrypt', '~> 3.1.11'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate', '1.0.0'
+gem 'bootstrap-sass', '>=3.3.7'
 # Rubocop
 gem 'rubocop'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_013549) do
+ActiveRecord::Schema.define(version: 2020_01_16_001552) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_12_06_013549) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
