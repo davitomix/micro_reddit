1dfb40a64edfd308c700951bfb4b158bdfc881f3
David Elì <davidelimg1@gmail.com>
2020-01-17 00:13:07 -0600

    Solved lint issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Micro Reddit App!"
+      flash[:success] = 'Welcome to Micro Reddit App!'
       redirect_to @user
     else
       render 'new'
